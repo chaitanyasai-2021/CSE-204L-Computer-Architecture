3b554ad725cb1ce1cb48b5a1ae9ea17940979c11
Chaitanya Sai Nutakki <91444457+chaitanyasai-2021@users.noreply.github.com>
2024-11-17 09:02:40 +0530

    Add files via upload

---
+++ Data warehousing and Mining (CSE 310L)/Week 1/create_textfile_static.py
@@ -0,0 +1,9 @@
+file = open("transactions.txt","w")
+
+file.write("T1-A B C D\n"
+     "T2-B C D\n"
+     "T3-A B C\n"
+     "T4-B C E\n"
+     "T5-A C D\n")
+
+file.close()
---
+++ Data warehousing and Mining (CSE 310L)/Week 2/Q1.py
@@ -0,0 +1,3 @@
+f=open('transactions3.txt','r')
+for each in f:
+    print (each)
